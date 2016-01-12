f680515123718dfb2573a7ddadbc544951ad58f6
Belousych <vas@elgrow.ru>
2016-01-12 08:57:07 +0500

    start

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/modernizr/modernizr.js
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/uikit/js/uikit.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
