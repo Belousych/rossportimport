8e070300a9ecee3484fefef2d41f466edfaaa28b
Belousych <vas@elgrow.ru>
2016-01-28 08:36:32 +0500

    new comm

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 //= ../../bower_components/modernizr/modernizr.js
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/uikit/js/uikit.js
+//= ../../bower_components/uikit/js/components/slider.js
 
 
 /*
